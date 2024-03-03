0bbcff24d14b518ab95a75e95c9772a5ee461338
Capataina <ata02caner@gmail.com>
2024-03-03 22:46:29 +0000

    checkout before merge

--- core/src/com/flappy/birb/FlappyBirb.java
+++ core/src/com/flappy/birb/FlappyBirb.java
@@ -1,31 +1,104 @@
 package com.flappy.birb;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class FlappyBirb extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import java.util.ArrayList;
+import java.util.List;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
+public class FlappyBirb extends Game {
+
+    private FlappyTheBirb bird;
+    private Ground ground;
+    private SpriteBatch batch;
+    private ShapeRenderer shapeRenderer;
+    private BitmapFont gameOverFont;
+    // private float numberOfCollisions;
+    private List<Pipes> listOfPipes;
+    private float timeSinceLastPipe;
+    private boolean gameEnded = false;
+
+    @Override
+    public void create() {
+        bird = new FlappyTheBirb(100, 300, 10, 900, 0); // Example position and size
+        ground = new Ground(0, 0);
+        shapeRenderer = new ShapeRenderer();
+        gameOverFont = new BitmapFont();
+        batch = new SpriteBatch();
+        listOfPipes = new ArrayList<>();
+        timeSinceLastPipe = 0;
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.31f, 0.73f, 0.77f, 1); // Clear the screen with black
+
+        timeSinceLastPipe += Gdx.graphics.getDeltaTime();
+
+        if (timeSinceLastPipe > 2) {  // Spawn every 2 seconds
+            listOfPipes.add(new Pipes(Gdx.graphics.getWidth(), (int) ground.getTopPoint()));  // Add a new pipe
+            timeSinceLastPipe = 0;
+        }
+
+        for (Pipes pipe : listOfPipes) {
+            pipe.update(Gdx.graphics.getDeltaTime());
+            pipe.draw(shapeRenderer);
+
+            if (hasCollidedWithUpperPipe(pipe) || hasCollidedWithBottomPipe(pipe)) {
+                GameEnd();
+            }
+        }
+
+        bird.draw(shapeRenderer); // Draw the bird as a yellow dot
+        ground.draw(shapeRenderer);
+
+        if (!gameEnded) {
+            bird.BirbControls(Gdx.graphics.getDeltaTime());
+        } else {
+            GameEnd();
+        }
+
+        if (hasCollidedWithGround()) {
+            GameEnd();
+        }
+
+    }
+
+    private boolean hasCollidedWithGround() {
+        // Using Intersector for more flexible overlap checks in the future
+        return Intersector.overlaps(bird.getBoundingCircle(), ground.getBoundingRect());
+    }
+
+    private boolean hasCollidedWithUpperPipe(Pipes pipe) {
+        return Intersector.overlaps(bird.getBoundingCircle(), pipe.UpperRectCollision());
+    }
+
+    private boolean hasCollidedWithBottomPipe(Pipes pipe) {
+        return Intersector.overlaps(bird.getBoundingCircle(), pipe.BottomRectCollision());
+    }
+
+    private void GameEnd() {
+        gameEnded = true;
+        shapeRenderer.end();
+        gameOverFont.getData().setScale(3.0f);
+        GlyphLayout layout = new GlyphLayout(gameOverFont, "You Died!");
+        float textWidth = layout.width;
+        batch.begin();
+        gameOverFont.draw(batch, "you ded lmao gg", (float) Gdx.graphics.getWidth() / 2 - textWidth, (float) Gdx.graphics.getHeight() / 2);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        shapeRenderer.dispose();
+        gameOverFont.dispose();
+        batch.dispose();
+    }
 }
--- core/src/com/flappy/birb/FlappyTheBirb.java
+++ core/src/com/flappy/birb/FlappyTheBirb.java
@@ -1,2 +1,57 @@
-package com.flappy.birb;public class FlappyTheBirb {
+package com.flappy.birb;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Vector2;
+
+public class FlappyTheBirb {
+    private Vector2 position;
+    private float radius; // Size of the bird represented as a dot
+    private float gravity;
+    private float birdVelocity;
+
+    public FlappyTheBirb(int x, int y, float radius, float gravity, float birdVelocity) {
+        position = new Vector2(x, y);
+        this.radius = radius; // Set the size of the dot
+        this.gravity = gravity;
+        this.birdVelocity = birdVelocity;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    // This method will be called from your render method in the main game screen
+    public void draw(ShapeRenderer shapeRenderer) {
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(1, 1, 0, 1); // Yellow color
+        shapeRenderer.circle(position.x, position.y, radius);
+        shapeRenderer.end();
+    }
+
+    public void BirbControls(float delta) {
+        float downwardAcceleration = gravity;
+        birdVelocity -= downwardAcceleration * delta;
+        birdVelocity *= 0.96f;
+
+        this.position.y += birdVelocity * delta;
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            birdVelocity += 500;
+        }
+
+        // System.out.println(birdVelocity);
+    }
+
+    // Helper method for getting the circular bounds of the bird
+    public Circle getBoundingCircle() {
+        return new Circle(position.x, position.y, radius);
+    }
+
 }
